d20cf2b6a171c2cc850793bab7a6f7186a2d864e
jitu23 <33281745+jitu23@users.noreply.github.com>
2018-01-16 18:36:10 +0530

    Add files via upload

---
+++ ConsoleApp2013/ConsoleApp2013/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2013
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CheckEmptyEntry(textBox1.Text) && CheckEmptyEntry(textBox2.Text))
+            {
+                if (CheckNumber(textBox1.Text) && CheckNumber(textBox2.Text))
+                    MessageBox.Show(AddNumber(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString());
+                else MessageBox.Show("Check entry");
+            }
+            else MessageBox.Show("Check entry");
+        }
+        public bool CheckEmptyEntry(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+            else return true;
+        }
+        public bool CheckNumber(string value)
+        {
+            double result;
+            if (double.TryParse(value, out result))
+                return true;
+            else return false;
+        }
+        public int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp2013/ConsoleApp2013Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2013;
+
+namespace ConsoleApp2013Test
+{
+    [TestClass]
+    public class TestCalculation
+    {
+        [TestMethod]
+        public void TestEmptyEntry()
+        {
+            Form1 f = new Form1();
+            Assert.IsTrue(f.CheckEmptyEntry(" "), "true");
+        }
+        [TestMethod]
+        public void TestNumbers()
+        {
+            Form1 f = new Form1();
+            Assert.IsTrue(f.CheckNumber("5.0"), "true");
+        }
+        [TestMethod]
+        public void TestAddition()
+        {
+            Form1 f = new Form1();
+            var result = f.AddNumber(10, 30);
+            Assert.AreEqual(40, result);
+        }
+    }
+}
